f39b60ba5cba40daefc50fbdd1f592779967e509
Ryan Farrell <rc0032044@student.riversidecollege.ac.uk>
2018-10-16 10:56:19 +0100

    salsdkdm

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.resistanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parallelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.potentialDifferenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(627, -108);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(355, 209);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 23;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(627, -108);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(355, 209);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(627, -108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(355, 209);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(270, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(355, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(284, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 19;
+            this.comboBox2.Text = "Please Select....";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(284, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 18;
+            this.comboBox1.Text = "Please Select....";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Show Formula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "1/Rt = 1/R1 + 1/R2 + 1/R3....";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Rt = R1 + R2 + R3....";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Show Formula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resistanceToolStripMenuItem,
+            this.potentialDifferenceToolStripMenuItem,
+            this.currentToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 13;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // resistanceToolStripMenuItem
+            // 
+            this.resistanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seriesToolStripMenuItem,
+            this.parallelToolStripMenuItem});
+            this.resistanceToolStripMenuItem.Name = "resistanceToolStripMenuItem";
+            this.resistanceToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.resistanceToolStripMenuItem.Text = "Resistance";
+            // 
+            // seriesToolStripMenuItem
+            // 
+            this.seriesToolStripMenuItem.Name = "seriesToolStripMenuItem";
+            this.seriesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.seriesToolStripMenuItem.Text = "Series";
+            this.seriesToolStripMenuItem.Click += new System.EventHandler(this.seriesToolStripMenuItem_Click_1);
+            // 
+            // parallelToolStripMenuItem
+            // 
+            this.parallelToolStripMenuItem.Name = "parallelToolStripMenuItem";
+            this.parallelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.parallelToolStripMenuItem.Text = "Parallel";
+            this.parallelToolStripMenuItem.Click += new System.EventHandler(this.parallelToolStripMenuItem_Click);
+            // 
+            // potentialDifferenceToolStripMenuItem
+            // 
+            this.potentialDifferenceToolStripMenuItem.Name = "potentialDifferenceToolStripMenuItem";
+            this.potentialDifferenceToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+            this.potentialDifferenceToolStripMenuItem.Text = "Potential Difference";
+            // 
+            // currentToolStripMenuItem
+            // 
+            this.currentToolStripMenuItem.Name = "currentToolStripMenuItem";
+            this.currentToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.currentToolStripMenuItem.Text = "Current";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(425, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(439, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(270, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem resistanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parallelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem potentialDifferenceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem currentToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
@@ -15,6 +14,138 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+            Hideall();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Hideall()
+        {
+            label1.Hide();
+            button1.Hide();
+            comboBox1.Hide();
+            label2.Hide();
+            button2.Hide();
+            comboBox2.Hide();
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            pictureBox4.Hide();
+            button3.Hide();
+            textBox1.Hide();
+            textBox2.Hide();
+
+
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            label1.Show();
+            await Task.Delay(500);
+            comboBox1.Show();
+            await Task.Delay(250);
+            comboBox1.Hide();
+            await Task.Delay(500);
+            comboBox1.Show();
+            await Task.Delay(250);
+            comboBox1.Hide();
+            await Task.Delay(500);
+            comboBox1.Show();
+
+        }
+        private void parellelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hideall();
+            button1.Show();
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+        }
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            label2.Show();
+            await Task.Delay(500);
+            comboBox2.Show();
+            await Task.Delay(250);
+            comboBox2.Hide();
+            await Task.Delay(500);
+            comboBox2.Show();
+            await Task.Delay(250);
+            comboBox2.Hide();
+            await Task.Delay(500);
+            comboBox2.Show();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                pictureBox1.Show();
+                textBox1.Show();
+                textBox2.Show();
+                button3.Show();
+
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                Hideall();
+                button1.Show();
+                label1.Show();
+                comboBox1.Show();
+                pictureBox2.Show();
+               
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                Hideall();
+                button1.Show();
+                label1.Show();
+                comboBox1.Show();
+                pictureBox3.Show();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                Hideall();
+                button1.Show();
+                label1.Show();
+                comboBox1.Show();
+                pictureBox4.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please select an option from the drop down menu!");
+            }
+        }
+
+        private void seriesToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Hideall();
+            button1.Show();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void parallelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hideall();
+            button2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double r1 = Convert.ToDouble(textBox1.Text);
+            double r2 = Convert.ToDouble(textBox2.Text);
+
+            r1 = r1 + r2;
+            label3.Text = Convert.ToString(r1);
         }
     }
 }
